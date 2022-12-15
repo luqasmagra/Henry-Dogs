ccc8a53b0e304cf1a647a66a7f15f15a558fc8a5
Lucas <luqasmagra@gmail.com>
2022-12-15 16:40:02 -0300

    (fix/cors-origin)

--- api/src/app.js
+++ api/src/app.js
@@ -19,9 +19,10 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Content-Range, Origin, X-Requested-With, Content-Type, Accept, Range"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Expose-Headers", "Content-Range");
   next();
 });
 
